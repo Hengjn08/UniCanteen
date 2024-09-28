5ba2373dbadf95f95c952cb773e140aeb9191197
Pierrelai99 <laijinhui99@gmail.com>
2024-09-28 20:01:11 +0800

    Merge branch 'master' of https://github.com/Hengjn08/UniCanteen
    # Conflicts:
    #	app/src/main/java/com/example/unicanteen/ChiaLiHock/FoodDetailsScreenCustomer.kt
    #	app/src/main/java/com/example/unicanteen/ChiaLiHock/SelectFoodScreen.kt
    #	app/src/main/java/com/example/unicanteen/LimSiangShin/Login.kt
    #	app/src/main/java/com/example/unicanteen/LimSiangShin/RegistrationScreen.kt
    #	app/src/main/java/com/example/unicanteen/ui/theme/AppViewModelProvider.kt

--- app/src/main/java/com/example/unicanteen/navigation/UniCanteenNavGraph.kt
+++ app/src/main/java/com/example/unicanteen/navigation/UniCanteenNavGraph.kt
@@ -87,7 +87,7 @@ fun UniCanteenNavHost(
 
     NavHost(
         navController = navController,
-        startDestination = SellerHomeDestination.route,      //应该最后要用login的,因为从那里开始,要test先放你们的第一页
+        startDestination = SelectRestaurantDestination.route,      //应该最后要用login的,因为从那里开始,要test先放你们的第一页
         modifier = modifier
     ) {
         Log.d("AppViewModelProvider", "Application context: $application")
